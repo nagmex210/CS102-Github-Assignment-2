b51afd0171111c51adce91940eb783cf49e27c70
Baris Yayci <barsyayc@gmail.com>
2023-09-20 15:20:21 +0300

    part e done

--- ArrayMenu.java
+++ ArrayMenu.java
@@ -1,4 +1,19 @@
 
 public class ArrayMenu {
 
+    private void findSumOdd(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i = i + 2) {
+            sum += array[i];
+        }
+        System.out.println("odd sum is : " + sum);
+    }
+
+    private void findSumEven(int[] array) {
+        int sum = 0;
+        for (int i = 1; i < array.length; i = i + 2) {
+            sum += array[i];
+        }
+        System.out.println("even sum is : " + sum);
+    }
 }
