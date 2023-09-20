5db6f2a6a0f5aeebcd24f54d056870e7267601dd
Koray Korkmaz <114093793+Koray-Korkmaz@users.noreply.github.com>
2023-09-20 15:16:32 +0300

    part c done ArrayMenu.java

--- ArrayMenu.java
+++ ArrayMenu.java
@@ -1,4 +1,21 @@
-
+public int findmax(int[] array){
+int max = array[0];
+for (int i = 0; i < array.length; i++) {
+    if(array[i]>max){
+        max = array[i];
+    }
+}
+return max;
+}
+public int findmin(int[] array){
+    int min = array[0];
+    for (int i = 0; i < array.length; i++) {
+        if(array[i]<min){
+            min = array[i];
+        }
+    }
+    return min;
+}
 public class ArrayMenu {
 
 }
